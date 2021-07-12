037c901c98d9800ae31b9483f74f3514c012f0cc
Jebakani <vjishwarya@gmail.com>
2021-07-12 12:52:44 +0530

    [JEBAKANI] P2- Searching the word in the file using binary search

---
+++ AlgorithmProblems/BinarySearchWord.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    class BinarySearchWord
+    {
+        string[] array;
+        public BinarySearchWord(string[] array)
+        {
+            this.array = array;
+            Array.Sort(this.array);
+        }
+        public int FindWord(string word,int low,int high)
+        {
+            int mid = low;
+            while (low < high)
+            {
+                mid = (high + low) / 2;
+                if (array[mid] == word)
+                {
+                    return 1;
+                }
+                else if (array[mid].CompareTo(word) > 0)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            if(array[mid]!=word)
+            {
+                return 0;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.IO;
 
 namespace AlgorithmProblems
 {
@@ -9,7 +10,8 @@ namespace AlgorithmProblems
         {
             Console.WriteLine("Welcome to Algorithm problems");
             Console.WriteLine("1.Permutation of string");
-            Console.WriteLine("2.Exit");
+            Console.WriteLine("2.Binary search for word");
+            Console.WriteLine("3.Exit");
             Console.Write("Enter your choice:");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch(choice)
@@ -23,7 +25,26 @@ namespace AlgorithmProblems
                         Console.WriteLine(arr);
                     }
                     break;
+
                 case 2:
+                    string filepath = @"C:\Users\HP1\source\repos\ALGORITHMPROBLEMS\AlgorithmProblems\AlgorithmFile.txt";
+                    string text = File.ReadAllText(filepath);
+                    string[] textArray = text.Split(" ");
+                    BinarySearchWord binarySearch = new BinarySearchWord(textArray);
+                    Console.WriteLine("Enter the word to search:");
+                    string word = Console.ReadLine();
+                    int found = binarySearch.FindWord(word, 0, textArray.Length - 1);
+                    if (found==1)
+                    {
+                        Console.WriteLine("{0} is found",word);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} is not found",word);
+                    }
+                    break;
+
+                case 3:
                     break;
             }
             Console.Read();
