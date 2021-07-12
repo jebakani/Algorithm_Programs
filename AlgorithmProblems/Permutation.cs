866c8c6094f5529a921145dbe5532ec0fbf57fff
Jebakani <vjishwarya@gmail.com>
2021-07-12 12:02:03 +0530

    [JEBAKANI] Printing all permutation of the string

---
+++ AlgorithmProblems/Permutation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    class Permutation 
+    {
+        ArrayList arrayList = new ArrayList();
+        //method to permute the string that is called recursively
+        public  void Permute(string str,int start,int end)
+        {
+            //if start and end point equals then the new string is formed
+            if(start==end)
+            {
+                arrayList.Add(str);
+            }
+            //else repeat the below process
+            else
+            {
+                //iterate using for loop
+                for(int i=start;i<=end;i++)
+                {
+                    //swap the start position with ith position
+                    str = Swap(str, start, i);
+                    //call permutate method with start as next position
+                    Permute(str, start + 1, end);
+                    //again swap the string with new start and ith position
+                    str = Swap(str, start, i);
+                }
+            }
+        }
+        public string Swap(string value,int i,int j )
+        {
+            //convert the string to char array
+            char[] charArray = value.ToCharArray();
+            char temp;
+            //swap the item at two position
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string str = new string(charArray);
+            //return the string
+            return str;
+        }
+        public ArrayList ReturnArray()
+        {
+            return arrayList;
+        }
+    }
+}
--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace AlgorithmProblems
 {
@@ -7,6 +8,24 @@ namespace AlgorithmProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm problems");
+            Console.WriteLine("1.Permutation of string");
+            Console.WriteLine("2.Exit");
+            Console.Write("Enter your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    Permutation permutation = new Permutation();
+                    permutation.Permute("abc",0,2);
+                    ArrayList array = permutation.ReturnArray();
+                    foreach(string arr in array)
+                    {
+                        Console.WriteLine(arr);
+                    }
+                    break;
+                case 2:
+                    break;
+            }
             Console.Read();
         }
     }
